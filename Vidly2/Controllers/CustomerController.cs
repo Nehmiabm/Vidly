0ad1d9fbf1b2627604812991688ebc47459010f1
Betsu <nehmiabm@gmail.com>
2016-07-20 21:24:07 -0500

    Exercise 1 finished

---
+++ Vidly2/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(GetCustomers());
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            var customers = new List<Customer>()
+            {
+                new Customer() {Name = "John Smith", Id = 1},
+                new Customer() {Name = "Mary Williams", Id = 2}
+            };
+            return customers;
+        }
+
+        public ActionResult Details(int id)
+        {
+          var customer=  GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly2/Controllers/MovieController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View(GetMovies());
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            var movies = new List<Movie>()
+            {
+               new Movie {Name = "Shrek"},
+               new Movie {Name="Wall-e" }
+            };
+            return movies;
+        }
+    }
+}
